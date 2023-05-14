a9030fa0491af48dc5659ddc3f29eadadf4bda7b
helpercorner <89002748+helpercorner@users.noreply.github.com>
2023-05-14 12:21:56 +0000

    0

--- main.sh
+++ main.sh
@@ -2,7 +2,7 @@ python3 main.py
 for (( i = 0; i < 50000; i = i + 1000))
 do 
     echo $FILE
-    python3 code.py "$i"
+    python3 app.py "$i"
     git add -A --verbose
     git commit -m "$i"
     git push

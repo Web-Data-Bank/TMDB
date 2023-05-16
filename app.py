7e8f412a7300092ae89d2a4771b617289aad9987
helpercorner <89002748+helpercorner@users.noreply.github.com>
2023-05-17 00:39:54 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,26 +1,27 @@
-import concurrent.futures
-import requests
+from __future__ import division
+from multiprocessing import Pool
+from requests import get
 import sys
 
+
 def MidSongLink(id):
-    url = "https://api.themoviedb.org/3/movie/{}?api_key=ed0646253701d7550481764a488b6ded".format(id)
-    with requests.Session() as session:
-        response = session.get(url, timeout=10)
-        response.raise_for_status()
-        with open("json/{}.json".format(id), 'wb') as file:
-            file.write(response.content)
+    url = "https://api.themoviedb.org/3/movie/" + id + "?api_key=ed0646253701d7550481764a488b6ded"
+    with get(url, timeout=10) as response:
+        content = response.content
+        with open("json/" + id + ".json", "wb") as file:
+            file.write(content)
 
 def main():
     a = int(sys.argv[1])
-    song_id = [str(i) for i in range(a, a + 1000)]
+    song_id = [str(i) for i in range(a, a+1000)]
 
+    pool_size = 250  # Adjust the pool size as per system capabilities
     num_tasks = len(song_id)
     print(num_tasks)
 
-    with concurrent.futures.ThreadPoolExecutor(max_workers=75) as executor:
-        futures = [executor.submit(MidSongLink, id) for id in song_id]
-        for i, _ in enumerate(concurrent.futures.as_completed(futures), 1):
+    with Pool(pool_size) as p:
+        for i, _ in enumerate(p.imap_unordered(MidSongLink, song_id), 1):
             sys.stderr.write('\rdone {0:%}'.format(i/num_tasks))
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()

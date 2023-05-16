cbf7a5527dcea5a87a2f01f283b4a7afbd6ce5c3
helpercorner <89002748+helpercorner@users.noreply.github.com>
2023-05-16 15:20:44 +0000

    Conflict Resolved

--- main.sh
+++ main.sh
@@ -1,8 +1,9 @@
-for (( i = 99000; i <= 100000; i = i + 250))
+for (( i = 100000; i <= 100250; i++))
 do 
-    echo $i
-    python3 app.py "$i"
-    git add -A --verbose
-    git commit -m "$i"
-    git push
+    rm json/$i.json
+    # echo $i
+    # python3 app.py "$i"
+    # git add -A --verbose
+    # git commit -m "$i"
+    # git push
 done

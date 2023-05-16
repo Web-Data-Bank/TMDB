8fb3b459f506b3219a3edb3e839227a213e0c711
helpercorner <89002748+helpercorner@users.noreply.github.com>
2023-05-17 00:41:45 +0530

    Update main.sh

--- main.sh
+++ main.sh
@@ -1,8 +1,9 @@
-for (( i = 350000; i < 400000; i = i + 1000))
+for (( i = 99000; i <= 100000; i = i + 250))
 do 
-    echo $i
-    python3 app.py "$i"
-    git add -A --verbose
-    git commit -m "$i"
-    git push
+    rm json/$i.json
+    # echo $i
+    # python3 app.py "$i"
+    # git add -A --verbose
+    # git commit -m "$i"
+    # git push
 done

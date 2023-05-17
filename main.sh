93267d9c0af7f361e2ec7de576dbfd6db84f0ca0
helpercorner <89002748+helpercorner@users.noreply.github.com>
2023-05-17 10:44:39 +0000

    Conflict Resolve

--- main.sh
+++ main.sh
@@ -1,9 +1,9 @@
-git config --global pack.threads "8"
-for (( i = 450000; i < 500000; i = i + 1000))
+for (( i = 99000; i <= 100000; i = i + 250))
 do 
-    echo $i
-    python3 app.py "$i"
-    git add -A --verbose
-    git commit -m "$i"
-    git push
+    rm json/$i.json
+    # echo $i
+    # python3 app.py "$i"
+    # git add -A --verbose
+    # git commit -m "$i"
+    # git push
 done

4f49af6392a6b56fda715ae6e624587aca11aa59
Venkatesh S <76198059+Venkatesh-KCET@users.noreply.github.com>
2023-05-17 16:29:33 +0530

    Update main.sh

--- main.sh
+++ main.sh
@@ -1,9 +1,8 @@
 for (( i = 99000; i <= 100000; i = i + 250))
 do 
-    rm json/$i.json
-    # echo $i
-    # python3 app.py "$i"
-    # git add -A --verbose
-    # git commit -m "$i"
-    # git push
+    echo $i
+    python3 app.py "$i"
+    git add -A --verbose
+    git commit -m "$i"
+    git push
 done

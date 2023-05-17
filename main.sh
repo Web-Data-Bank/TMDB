d29b5ba9ca3a12171cd4ab2521c7434d37dce490
Venkatesh S <76198059+Venkatesh-KCET@users.noreply.github.com>
2023-05-17 11:37:05 +0000

    550000

--- main.sh
+++ main.sh
@@ -1,9 +1,10 @@
-for (( i = 99000; i <= 100000; i = i + 250))
+git config --global pack.threads "8"
+for (( i = 550000; i <= 700000; i = i + 2500))
 do 
-    rm json/$i.json
-    # echo $i
-    # python3 app.py "$i"
-    # git add -A --verbose
-    # git commit -m "$i"
-    # git push
+    # rm json/$i.json
+    echo $i
+    python3 app.py "$i"
+    git add -A --verbose
+    git commit -m "$i"
+    git push
 done

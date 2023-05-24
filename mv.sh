7f1eabf9a4ec291f0b53cb435b7bf3c7f3977fbd
Venkatesh S <76198059+Venkatesh-KCET@users.noreply.github.com>
2023-05-24 04:45:36 +0000

    SH Files Removed

--- mv.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-folder="TMDB/json"  # Replace with the actual folder path
-
-# Loop through the files in the folder
-for file in "$folder"/*; do
-    # echo "$file"
-    mv $file json/
-done

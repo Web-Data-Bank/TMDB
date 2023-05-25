41f27fec3764db74cc6a6e25087cb23be9b4a133
Venkatesh S <20ucs085@kamarajengg.edu.in>
2023-05-25 07:15:10 +0000

    AT

--- alternative_title.py
+++ alternative_title.py
@@ -5,11 +5,12 @@ from requests import get
 import sys
 
 def Download_API(id):
-    url = "https://api.themoviedb.org/3/movie/" + id + "/alternative_titles?api_key=ed0646253701d7550481764a488b6ded"
-    with get(url, timeout=10) as response:
-        content = response.content
-        with open("alternative_titles/" + id + ".json", "wb") as file:
-            file.write(content)
+    if not os.path.exists("alternative_titles/" + id + ".json"):
+        url = "https://api.themoviedb.org/3/movie/" + id + "/alternative_titles?api_key=ed0646253701d7550481764a488b6ded"
+        with get(url, timeout=10) as response:
+            content = response.content
+            with open("alternative_titles/" + id + ".json", "wb") as file:
+                file.write(content)
 
 file_path = 'file_names.txt'
 

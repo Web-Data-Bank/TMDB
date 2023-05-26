4e60e242496ebc5c7eb2d3fe079161636b7f2232
Venkatesh S <76198059+Venkatesh-KCET@users.noreply.github.com>
2023-05-26 20:31:25 +0530

    Create main.sh

---
+++ test/main.sh
@@ -0,0 +1,10 @@
+git config --global pack.threads "8"
+for (( i = 1; i < 188; i++))
+do 
+    # rm json/$i.json
+    echo $i
+    python3 main.py "$i"
+    git add -A --verbose
+    git commit -m "Added alternative_title $i"
+    git push
+done
